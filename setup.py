1d0899c8b6ffdaa0310951c620601eb32e8b021e
KimSeongjin72 <126738149+KimSeongjin72@users.noreply.github.com>
2023-04-11 12:54:21 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name = "seongjin0702",
+    version = '0.0.1',
+    description = 'my calculator',
+    author = 'seong jin',
+    url = 'https://github.com/KimSeongjin72/calc0001',
+    download_url = 'https://github.com/KimSeongjin72/calc0001/archive/refs/tags/v0.0.1.zip',
+    packages = ['mycalc'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
